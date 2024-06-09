6bf1752abd25956435d715e992580844dd869b07
marvol0 <murithimarvin09@gmail.com>
2024-06-09 13:05:04 +0300

    feat: implement decryption in caesar cipher

--- Caesar Cipher/main.py
+++ Caesar Cipher/main.py
@@ -13,4 +13,15 @@ def encrypt(plain_text, shift_amount):
         cipher_text += new_letter
     print(f"The encoded text is {cipher_text}")
 
-encrypt(plain_text=text, shift_amount=shift)
+def decrypt(cipher_text, shift_amount):
+    plain_text = ""
+    for letter in cipher_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        plain_text += alphabet[new_position]
+    print(f"The decoded text is {plain_text}")
+
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+    decrypt(cipher_text=text, shift_amount=shift)

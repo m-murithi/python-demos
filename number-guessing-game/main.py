f276e9b53097d511e9cab9c1a7385159f305e55d
marvol0 <murithimarvin09@gmail.com>
2024-06-10 13:24:46 +0300

    feat: implement the guessing logic

--- number-guessing-game/main.py
+++ number-guessing-game/main.py
@@ -22,3 +22,26 @@ def set_difficulty():
     else:
         return HARD_LEVEL_TURNS
     
+def game():
+    print(logo)
+    print("Welcome to the Number Guessing Game!")
+    print("I'm thinking of a number between 1 and 100")
+    answer = randint(1, 100)
+    print(f"Pssst, the correct answer is {answer}")
+    
+    turns = set_difficulty()
+    
+    guess = 0 
+    while guess != answer:
+        print(f"You have {turns} attempts remaining to guess the number")
+        
+        guess = int(input("Make a guess: "))
+        
+        turns = check_answer(guess, answer, turns)
+        if turns == 0:
+            print("You've run out of guesses, you lose.")
+            return
+        elif guess != answer:
+            print("Guess again")
+            
+game()

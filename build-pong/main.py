522ae19e90f203f0588518f7824a73c26c6c9b0e
marvol0 <murithimarvin09@gmail.com>
2024-06-19 15:41:53 +0300

    feat: add ball class and make ball move

--- build-pong/main.py
+++ build-pong/main.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 screen.bgcolor("black")
@@ -9,6 +10,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
+ball = Ball()
 
 
 screen.listen()
@@ -20,3 +22,4 @@ screen.onkey(l_paddle.go_down, "s")
 game_is_on = True
 while game_is_on:
     screen.update()
+    ball.move()

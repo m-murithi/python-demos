d5cad690bf031013406ab373d08f672260eba879
marvol0 <murithimarvin09@gmail.com>
2024-06-19 15:55:17 +0300

    feat: add score keeping

--- build-pong/main.py
+++ build-pong/main.py
@@ -1,6 +1,8 @@
 from turtle import Screen, Turtle
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
+import time
 
 screen = Screen()
 screen.bgcolor("black")
@@ -11,7 +13,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
-
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
@@ -35,9 +37,11 @@ while game_is_on:
     #Detect R paddle misses
     if ball.xcor() > 380:
         ball.reset_position()
-        
+        scoreboard.l_point()
 
     #Detect L paddle misses:
     if ball.xcor() < -380:
         ball.reset_position()
-        
+        scoreboard.r_point()
+
+screen.exitonclick()

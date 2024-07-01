a28299de0705989966560bf746683633ee480c50
marvol0 <murithimarvin09@gmail.com>
2024-07-01 13:16:44 +0300

    feat: add scoreboard and game over sequence

--- turtle-crossing/main.py
+++ turtle-crossing/main.py
@@ -2,6 +2,7 @@ import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +10,7 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -21,15 +23,15 @@ while game_is_on:
     car_manager.create_car()
     car_manager.move_cars()
 
-    
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
 
-    
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
+        scoreboard.increase_level()
 
 
 screen.exitonclick()

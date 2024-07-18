3ad9e96e911d3d0c03d9000da1d5431b366e6112
marvol0 <murithimarvin09@gmail.com>
2024-07-18 12:44:41 +0300

    feat: setting up canvas

---
+++ password-manager/main.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+canvas = Canvas(height=200, width=200)
+logo_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=logo_img)
+canvas.grid(row=0, column=1)

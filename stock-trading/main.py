9802ce3640c6a33d609c47305ff8bb7f8bc14727
marvol0 <murithimarvin09@gmail.com>
2024-08-07 09:57:35 +0300

    feat: intergrate twilio to send sms

--- stock-trading/main.py
+++ stock-trading/main.py
@@ -1,3 +1,10 @@
+import requests
+from twilio.rest import Client
+
+VIRTUAL_TWILIO_NUMBER = ""
+VERIFIED_NUMBER = ""
+
+
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
@@ -6,6 +13,8 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = ""
 NEWS_API_KEY = ""
+TWILIO_SID = ""
+TWILIO_AUTH_TOKEN = ""
 
 stock_params = {
     "function": "TIME_SERIES_DAILY",
@@ -25,6 +34,11 @@ day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
 print(day_before_yesterday_closing_price)
 
 difference = float(yesterday_closing_price) - float(day_before_yesterday_closing_price)
+up_down = None
+if difference > 0:
+    up_down = "🔺"
+else:
+    up_down = "🔻"
 
 diff_percent = round((difference / float(yesterday_closing_price)) * 100)
 print(diff_percent)
@@ -40,4 +54,15 @@ if abs(diff_percent) > 1:
 
     three_articles = articles[:3]
     print(three_articles)
+
+    formatted_articles = [f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline: {article['title']}. \nBrief: {article['description']}" for article in three_articles]
+    print(formatted_articles)
+    client = Client(TWILIO_SID, TWILIO_AUTH_TOKEN)
+
+    for article in formatted_articles:
+        message = client.messages.create(
+            body=article,
+            from_=VIRTUAL_TWILIO_NUMBER,
+            to=VERIFIED_NUMBER
+        )
 

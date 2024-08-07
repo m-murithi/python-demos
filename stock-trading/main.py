3fdc09f755149799e7be080874643fd0a79e297c
marvol0 <murithimarvin09@gmail.com>
2024-08-07 09:49:03 +0300

    feat: get news articles

--- stock-trading/main.py
+++ stock-trading/main.py
@@ -5,6 +5,7 @@ STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 STOCK_API_KEY = ""
+NEWS_API_KEY = ""
 
 stock_params = {
     "function": "TIME_SERIES_DAILY",
@@ -27,3 +28,16 @@ difference = float(yesterday_closing_price) - float(day_before_yesterday_closing
 
 diff_percent = round((difference / float(yesterday_closing_price)) * 100)
 print(diff_percent)
+
+if abs(diff_percent) > 1:
+    news_params = {
+        "apiKey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
+
+    news_response = requests.get(NEWS_ENDPOINT, params=news_params)
+    articles = news_response.json()["articles"]
+
+    three_articles = articles[:3]
+    print(three_articles)
+

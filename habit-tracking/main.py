9265eab80e24cf7f431aaf3d91e42ad63f349087
marvol0 <murithimarvin09@gmail.com>
2024-08-07 10:17:20 +0300

    feat: authenticating using HTTP header

---
+++ habit-tracking/main.py
@@ -0,0 +1,32 @@
+import requests
+
+USERNAME = ""
+TOKEN = ""
+GRAPH_ID = ""
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id": GRAPH_ID,
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+print(response.text)
+

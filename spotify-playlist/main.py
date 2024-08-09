3f10a18bf0b5d0a746b1fa00749b3e24fd57d5d0
marvol0 <murithimarvin09@gmail.com>
2024-08-09 11:40:35 +0300

    feat: add spotify authentication

--- spotify-playlist/main.py
+++ spotify-playlist/main.py
@@ -1,11 +1,25 @@
-
 from bs4 import BeautifulSoup
 import requests
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
 
+# Scraping Billboard 100
 date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
-
 response = requests.get("https://www.billboard.com/charts/hot-100/" + date)
-
 soup = BeautifulSoup(response.text, 'html.parser')
 song_names_spans = soup.select("li ul li h3")
 song_names = [song.getText().strip() for song in song_names_spans]
+
+#Spotify Authentication
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=YOUR CLIENT ID,
+        client_secret=YOUR CLIENT SECRET,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+print(user_id)

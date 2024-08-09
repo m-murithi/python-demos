e85cead7189dcebb44fb1dabc8532885a2d8f4ac
marvol0 <murithimarvin09@gmail.com>
2024-08-09 11:52:58 +0300

    feat: send notification using smtp

--- price-tracker/main.py
+++ price-tracker/main.py
@@ -1,5 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
+import smtplib
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 url = "https://www.amazon.com/dp/B075CYMYK6?psc=1&ref_=cm_sw_r_cp_ud_ct_FM9M699VKHTT47YD50Q6"
 
@@ -32,4 +38,11 @@ BUY_PRICE = 70
 if price_as_float < BUY_PRICE:
     message = f"{title} is on sale for {price}!"
 
-    
+    with smtplib.SMTP(os.environ["SMTP_ADDRESS"], port=587) as connection:
+        connection.starttls()
+        result = connection.login(os.environ["EMAIL_ADDRESS"], os.environ["EMAIL_PASSWORD"])
+        connection.sendmail(
+            from_addr=os.environ["EMAIL_ADDRESS"],
+            to_addrs=os.environ["EMAIL_ADDRESS"],
+            msg=f"Subject:Amazon Price Alert!\n\n{message}\n{url}".encode("utf-8")
+        )

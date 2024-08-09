89257db3007e8edfe3c1ec8c512af384846a8ffb
marvol0 <murithimarvin09@gmail.com>
2024-08-09 11:51:20 +0300

    feat: set price to get notification

--- price-tracker/main.py
+++ price-tracker/main.py
@@ -12,3 +12,24 @@ response = requests.get(url, headers=header)
 soup = BeautifulSoup(response.content, "html.parser")
 print(soup.prettify())
 
+# Find the HTML element that contains the price
+price = soup.find(class_="a-offscreen").get_text()
+
+# Remove the dollar sign using split
+price_without_currency = price.split("$")[1]
+
+# Convert to floating point number
+price_as_float = float(price_without_currency)
+print(price_as_float)
+
+# Get the product title
+title = soup.find(id="productTitle").get_text().strip()
+print(title)
+
+# Set the price below which you would like to get a notification
+BUY_PRICE = 70
+
+if price_as_float < BUY_PRICE:
+    message = f"{title} is on sale for {price}!"
+
+    
